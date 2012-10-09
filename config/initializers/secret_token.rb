352b4c6428214177c606c890afe0b339a26e7f58
Hélder Moreira <heldersantos.moreira@gmail.com>
2012-10-09 23:09:13 +0100

    Commit inicial. Criada aplicacao e scaffold para os users.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrainCompanyServer::Application.config.secret_token = '8c5643bb02f19e074153f900091ddaa957256a8d514fba5eb183023e76701f31502350ac196918a2d7010b17380d295572beb047deeea6e4c1f80b0d0f215961'
