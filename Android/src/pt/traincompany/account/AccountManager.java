64d49d957caf856288442ba31cd0e3ed6675e6c6
Hélder Moreira <heldersantos.moreira@gmail.com>
2012-10-31 00:03:33 +0000

    adicionada atividade para gestao de cartoes. versao inicial.

--- Android/src/pt/traincompany/account/AccountManager.java
+++ Android/src/pt/traincompany/account/AccountManager.java
@@ -3,7 +3,10 @@ package pt.traincompany.account;
 import pt.traincompany.main.R;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class AccountManager extends Activity {
 
@@ -11,6 +14,14 @@ public class AccountManager extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_manager);
+        
+        final Button login = (Button) findViewById(R.id.btnLogin);
+        login.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent myIntent = new Intent(AccountManager.this, CardManagement.class);
+				AccountManager.this.startActivity(myIntent);
+			}
+		});
     }
 
     @Override
---
+++ Android/src/pt/traincompany/account/CardManagement.java
@@ -0,0 +1,54 @@
+package pt.traincompany.account;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import pt.traincompany.main.R;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class CardManagement extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_management);
+        
+        ListView list = (ListView) findViewById(R.id.creditCards);
+        
+        ArrayList<HashMap<String, String>> mylist = new ArrayList<HashMap<String, String>>();
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("creditCardNumber", "123456");
+        mylist.add(map);
+        map = new HashMap<String, String>();
+        map.put("creditCardNumber", "654321");
+        mylist.add(map);
+
+        SimpleAdapter adapter = new SimpleAdapter(this, mylist, R.layout.creditcard_row,
+                    new String[] {"creditCardNumber", "removeCard"}, new int[] {R.id.creditCardNumber, R.id.removeCard});
+        list.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_card_management, menu);
+        return true;
+    }
+    
+    public void removeHandler(View v) 
+    {
+
+        LinearLayout vwParentRow = (LinearLayout)v.getParent();
+        TextView child = (TextView)vwParentRow.getChildAt(0);
+        Toast.makeText(getApplicationContext(), child.getText(),
+				Toast.LENGTH_SHORT).show();
+      
+    }
+}
