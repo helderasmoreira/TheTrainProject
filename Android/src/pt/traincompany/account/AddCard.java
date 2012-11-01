8b688a5e0ff372ddeb0e3f4a79ae9544b24467b1
Hélder Moreira <heldersantos.moreira@gmail.com>
2012-11-01 14:51:01 +0000

    adicionada a atividade para adicionar um cartao

---
+++ Android/src/pt/traincompany/account/AddCard.java
@@ -0,0 +1,46 @@
+package pt.traincompany.account;
+
+import java.lang.reflect.Field;
+
+import pt.traincompany.main.R;
+import pt.traincompany.main.R.layout;
+import pt.traincompany.main.R.menu;
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.DatePicker;
+
+public class AddCard extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_card);
+
+		DatePicker picker = (DatePicker) findViewById(R.id.expiryDate);
+		try {
+			Field f[] = picker.getClass().getDeclaredFields();
+			for (Field field : f) {
+				if (field.getName().equals("mDayPicker")) {
+					field.setAccessible(true);
+					Object dayPicker = field.get(picker);
+					((View) dayPicker).setVisibility(View.GONE);
+				}
+			}
+		} catch (SecurityException e) {
+			Log.d("ERROR", e.getMessage());
+		} catch (IllegalArgumentException e) {
+			Log.d("ERROR", e.getMessage());
+		} catch (IllegalAccessException e) {
+			Log.d("ERROR", e.getMessage());
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_add_card, menu);
+		return true;
+	}
+}
