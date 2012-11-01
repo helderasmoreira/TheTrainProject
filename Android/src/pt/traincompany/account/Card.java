01e1166e2f929b5ac6d8df8042d67ff50cd327ff
Hélder Moreira <heldersantos.moreira@gmail.com>
2012-11-01 12:29:54 +0000

    refeito layout da atividade de gestao de cartoes.

---
+++ Android/src/pt/traincompany/account/Card.java
@@ -0,0 +1,15 @@
+package pt.traincompany.account;
+
+public class Card {
+    public String number;
+    
+    public Card(){
+        super();
+    }
+    
+    public Card(String number) {
+        super();
+        this.number = number;
+    }
+
+}
